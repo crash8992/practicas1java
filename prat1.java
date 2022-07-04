42dc73925d6f5559c3e1cf52f9a5f5ccbe552741
crash899 <crash899@gmail.com>
2022-07-04 08:56:58 -0500

    cambio archivo subida

---
+++ prat1.java
@@ -0,0 +1,15 @@
+public class prat1{
+    public static void main(String args[]){
+        int miVariableEntera =10;
+        System.out.println(miVariableEntera);
+    }
+
+
+    private String holamundo;
+
+    
+    String saludo =holamundo;
+    System.out.println(holamundo);
+
+
+}
